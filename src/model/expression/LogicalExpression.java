ad3ddd03866cea1dc4320a10263959231118641b
maiaiia <176808387+maiaiia@users.noreply.github.com>
2025-10-20 10:54:42 +0300

    implement LogicalExpression

--- src/model/expression/LogicalExpression.java
+++ src/model/expression/LogicalExpression.java
@@ -1,15 +1,37 @@
 package model.expression;
 
+import exception.InvalidOperandTypeException;
+import exception.InvalidOperatorException;
+import model.value.BooleanValue;
 import model.value.IValue;
+import model.value.Type;
 import state.IExecutionStack;
 import state.ISymbolTable;
 
 public class LogicalExpression implements IExpression {
-    IExpression lhs;
-    IExpression rhs;
+    IExpression left;
+    IExpression right;
+    String operator;
+
+    boolean getBoolValue(IExpression expression, ISymbolTable symbolTable) throws InvalidOperandTypeException{
+        IValue value = expression.evaluate(symbolTable);
+        if(! (value.getType() == Type.BOOLEAN)){
+            throw new InvalidOperandTypeException();
+        }
+        return ((BooleanValue)value).getValue();
+    }
 
     @Override
-    public IValue evaluate(ISymbolTable symbolTable) {
-        return null;
+    public IValue evaluate(ISymbolTable symbolTable) throws InvalidOperandTypeException {
+        boolean leftBoolValue = getBoolValue(left, symbolTable);
+        boolean rightBoolValue = getBoolValue(right, symbolTable);
+
+        boolean result = switch (operator) {
+            case "and" -> leftBoolValue && rightBoolValue;
+            case "or" -> leftBoolValue || rightBoolValue;
+            case "xor" -> leftBoolValue ^ rightBoolValue;
+            default -> throw new InvalidOperatorException();
+        };
+        return new BooleanValue(result);
     }
 }
