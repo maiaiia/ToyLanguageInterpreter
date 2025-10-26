5864b190a086f1728c451e77b73617e5e1f74ed7
maiaiia <176808387+maiaiia@users.noreply.github.com>
2025-10-26 13:26:52 +0200

    add value expression

---
+++ src/model/expression/ValueExpression.java
@@ -0,0 +1,18 @@
+package model.expression;
+
+
+import model.value.IValue;
+import state.ISymbolTable;
+
+public class ValueExpression implements IExpression {
+    private final IValue value;
+
+    public ValueExpression(IValue value) {
+        this.value = value;
+    }
+
+    @Override
+    public IValue evaluate(ISymbolTable symbolTable) {
+        return value;
+    }
+}
