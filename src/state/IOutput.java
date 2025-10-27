3194f22d67b57534cc059863e1d61568fbc44737
maiaiia <176808387+maiaiia@users.noreply.github.com>
2025-10-27 09:47:08 +0200

    fix state output

--- src/state/IOutput.java
+++ src/state/IOutput.java
@@ -1,7 +1,5 @@
 package state;
 
-import model.value.IValue;
-
 public interface IOutput {
-    void append(IValue value);
+    void append(String s);
 }
--- src/state/ListOutput.java
+++ src/state/ListOutput.java
@@ -1,16 +1,17 @@
 package state;
 
+import model.expression.IExpression;
 import model.value.IValue;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ListOutput implements IOutput {
-    private final List<IValue> values = new ArrayList<>();
+    private final List<String> outputs = new ArrayList<>();
 
     @Override
-    public void append(IValue value) {
-        values.addLast(value);
+    public void append(String string) {
+        outputs.addLast(string);
     }
 }
 
